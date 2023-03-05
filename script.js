6d10f80de1482a132929dff6b4bc9ed6fa5e51aa
AleruDivine <alerudivine@gmail.com>
2023-03-05 13:44:15 +0100

    second commit

--- script.js
+++ script.js
@@ -20,3 +20,17 @@ const calcAmplitude = function (temps) {
 };
 const amplitude = calcAmplitude([3, -2, -6, -1, 'error', 9, 13]);
 console.log(amplitude);
+
+//coding challenge 
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+const printForecast = function(arr){
+  let str = '';
+  for (let i = 0; i < arr.length; i++){
+str = str + `${arr[i]}c in ${i + 1} days ... `;
+  }
+  console.log('...' + str);
+}
+printForecast(data1)
